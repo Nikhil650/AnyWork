d05648831298edecac5b1d3f5ede54e767a8fb2e
Nikhil Agarwal <nikhilagarwal@jklu.edu.in>
2021-06-21 17:46:11 +0530

    connecting backend

--- src/SignIn_Up/AccountBox/loginform.jsx
+++ src/SignIn_Up/AccountBox/loginform.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import {
   BoldLink,
   BoxContainer,
@@ -7,22 +7,38 @@ import {
   MutedLink,
   SubmitButton,
 } from "./common";
+import axios from "axios";
 import { Marginer } from "../Marginer";
 import { AccountContext } from "./accountContext";
 
 export function LoginForm(props) {
   const { switchToSignup } = useContext(AccountContext);
+  const [input,setInput] = useState({Email: "", Password: ""});
+  const {Email, Password} = input;
+
+  function handleChange(e){
+    setInput({
+      ...input,
+      [e.target.name] : e.target.value
+    })
+  }
+
+  function Submit(e){
+    e.preventDefault()
+    const data = {Email, Password}
+    axios.post("http://localhost:5001/register/signup",data)
+  }
 
   return (
     <BoxContainer>
       <FormContainer>
-        <Input type="email" placeholder="Email" />
-        <Input type="password" placeholder="Password" />
+        <Input type="email" placeholder="Email" value={Email} name="Email" onChange={handleChange} />
+        <Input type="password" placeholder="Password" value={Password} name="Password" onChange={handleChange} />
       </FormContainer>
       <Marginer direction="vertical" margin={10} />
       <MutedLink href="#">Forget your password?</MutedLink>
       <Marginer direction="vertical" margin="1.6em" />
-      <SubmitButton type="submit">Signin</SubmitButton>
+      <SubmitButton type="submit" onClick={Submit}>Signin</SubmitButton>
       <Marginer direction="vertical" margin="1em" />
       <MutedLink href="#">
         Don't have an accoun?{" "}
--- src/SignIn_Up/AccountBox/signupForm.jsx
+++ src/SignIn_Up/AccountBox/signupForm.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import {
   BoldLink,
   BoxContainer,
@@ -7,23 +7,39 @@ import {
   MutedLink,
   SubmitButton,
 } from "./common";
+import axios from "axios";
 import { Marginer } from "../Marginer";
 import { AccountContext } from "./accountContext";
 
 export function SignupForm(props) {
   const { switchToSignin } = useContext(AccountContext);
+  const [input,setInput] = useState({First_Name: "", Last_Name: "", Email: "", Password: ""});
+  const {First_Name, Last_Name, Email, Password} = input;
+  
+  function handleChange(e){
+    setInput({
+      ...input,
+      [e.target.name] : e.target.value
+    })
+  }
+
+  function Submit(e){
+    e.preventDefault()
+    const data = {First_Name, Last_Name, Email, Password}
+    axios.post("http://localhost:5001/register/signup",data)
+  }
 
   return (
     <BoxContainer>
       <FormContainer>
-        <Input type="text" placeholder="First Name" />
-        <Input type="text" placeholder="Last Name" />
-        <Input type="email" placeholder="Email" />
-        <Input type="password" placeholder="Password" />
+        <Input type="text" placeholder="First Name" value={First_Name} name="First_Name" onChange={handleChange} />
+        <Input type="text" placeholder="Last Name" value={Last_Name} name="Last_Name" onChange={handleChange} />
+        <Input type="email" placeholder="Email" value={Email} name="Email" onChange={handleChange} />
+        <Input type="password" placeholder="Password" value={Password} name="Password" onChange={handleChange} />
         <Input type="password" placeholder="Confirm Password" />
       </FormContainer>
       <Marginer direction="vertical" margin={10} />
-      <SubmitButton type="submit">Signup</SubmitButton>
+      <SubmitButton type="submit" onClick={Submit}>Signup</SubmitButton>
       <Marginer direction="vertical" margin="1em" />
       <MutedLink href="#">
         Already have an account?
